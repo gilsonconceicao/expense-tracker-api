78539936b9174188d9115c053a47c91961b0eda0
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2026-08-18 23:35:41 -0300

    feat: use begin transaction when add transaction

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -10,30 +10,35 @@ class TransactionController < ApplicationController
   end
 
   def create
-    @params_create = transaction_params_create
+    @params_create = transaction_model_create
     @new_transaction = Transaction.new(@params_create)
-
-    if !@params_create[:date_at].blank?
-      @new_transaction.date_at = @params_create[:date_at]
-    else
-      @new_transaction.date_at = Date.current
-    end
 
     if !@params_create[:purpose].blank? && (@params_create[:purpose] > 2 || @params_create[:purpose] == 0)
       render json: { error: "Send a valid value to purpose field between 1 - incoming or 2 - outgoing" }, status: :bad_request and return
     end
 
-    if @new_transaction.save
-      render json: { new_transaction: @new_transaction }, status: :created
-    else
-      render json: @new_transaction.errors, status: :unprocessable_entity and return
+    Transaction.transaction do
+      if !@params_create[:date_at].blank?
+        @new_transaction.date_at = @params_create[:date_at]
+      else
+        @new_transaction.date_at = Date.current
+      end
+
+      @user = User.find(@params_create[:user_id])
+      @new_transaction.user_id = @user.id
+
+      if @new_transaction.save
+        render json: { new_transaction: @new_transaction }, status: :created
+      else
+        render json: @new_transaction.errors, status: :unprocessable_entity and return
+      end
     end
   end
 
   def update
     @transaction = Transaction.find(params[:id])
 
-    if @transaction.update(transaction_params_update)
+    if @transaction.update(transaction_model_update)
       render json: { updated_transaction: @transaction }, status: :ok
     else
       render json: @transaction.errors, status: :unprocessable_entity and return
@@ -47,12 +52,12 @@ class TransactionController < ApplicationController
   end
 
   private
-  def transaction_params_create
-    params.require(:transaction).permit(:amount, :description, :date_at, :purpose)
+  def transaction_model_create
+    params.require(:transaction).permit(:amount, :description, :date_at, :purpose, :user_id)
   end
 
   private
-  def transaction_params_update
+  def transaction_model_update
     params.require(:transaction).permit(:amount, :description, :purpose)
   end
 end
